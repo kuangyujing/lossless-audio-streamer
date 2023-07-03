f4bf12dfbee22989aae3acd163b309951ea32cdb
Kuangyu Jing <kuangyujing@appsworks.jp>
2023-07-04 06:51:50 +0900

    Upload and encoding

--- server.js
+++ server.js
@@ -10,15 +10,23 @@ const MYSQLDATABASE = process.env.MYSQLDATABASE
 const MYSQLUSER = process.env.MYSQLUSER
 const MYSQLPASSWORD = process.env.MYSQLPASSWORD
 
+const filestore = 'filestore'
 const multer = require('multer')
-const upload = multer({ dest: 'filestore/' })
+const upload = multer({ dest: filestore })
 
-app.listen(port, function () {
-    console.log("Express listening on port " + port)
-})
+app.listen(port, () => console.log("Express listening on port " + port))
+app.use(express.static('public'))
+app.use(express.json())
+
+//const jsonParser = bodyParser.json()
+//const bodyParser = require('body-parser')
+//app.use(bodyParser.urlencoded({
+//    extended: true
+//}));
+//app.use(bodyParser.json());
 
 // for testing streaming plain mp3
-app.get('/api/play/:key', function (req, res) {
+app.get('/api/play/:key', (req, res) => {
     console.log("Received request to play " + req.params.key + ".mp3")
     const key = req.params.key
 
@@ -67,7 +75,7 @@ app.get('/api/play/:key', function (req, res) {
 })
 
 // for testing userid
-app.get('/api/play/:userid/:key', function (req, res) {
+app.get('/api/play/:userid/:key', (req, res) => {
     console.log("Received request to play " + req.params.key + ".mp3")
     const key = req.params.key
 
@@ -116,7 +124,7 @@ app.get('/api/play/:userid/:key', function (req, res) {
 })
 
 // incomplete: for testing start/end
-app.get('/api/play/:key/:a/:b', function (req, res) {
+app.get('/api/play/:key/:a/:b', (req, res) => {
     console.log("Received request to play " + req.params.key + ".mp3")
     const key = req.params.key
 
@@ -166,8 +174,39 @@ app.get('/api/play/:key/:a/:b', function (req, res) {
     readStream.pipe(res)
 })
 
-app.post('/upload', upload.single('file'), (req, res) => res.send(req.file.filename))
+app.post('/upload', upload.single('file'), (req, res) => {
+    console.log(`Received request to upload ${req.file.filename}`)
+    // add .mp3 extension since simple upload does not have it
+    fs.renameSync(req.file.path, req.file.path + '.mp3')
+    res.send(req.file.filename + '.mp3')
+})
 
+app.post('/convert/:filename', (req, res) => {
+    const filename = req.params.filename
+    // get start and end time from request body
+    const start = req.body.start
+    const end = req.body.end
+    console.log(`Received request to convert ${filename} from ${start} to ${end}`)
+
+    if (!start || !end) {
+        res.status(400).send('Bad request')
+        return
+    }
+
+    // mute part of mp3 from `start` sec to `end` sec and save as `filename`-muted.mp3
+    const batch = 'ffmpeg -i ' + filestore + '/' + filename + '.mp3' + ' -af "volume=enable=\'between(t,' + start + ',' + end + ')\':volume=0" ' + filestore + '/' + filename + '-muted.mp3'
+    // execute command in bash
+    const exec = require('child_process').exec
+    exec(batch, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(stdout)
+        console.log(stderr)
+    })
+    res.send('OK')
+})
 
 app.get('/', function (req, res) {
     res.send("OK")
